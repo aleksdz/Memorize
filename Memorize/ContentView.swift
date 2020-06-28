8ae9e40e61adf15c5878bf25826daded4b7208a9
Aleks <zaersx@gmail.com>
2020-06-28 10:40:04 +0100

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Aleks Nazarenko on 28.06.20.
+//  Copyright © 2020 Aleks Nazarenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
